235d50bfd6866e5caac1663b3b320b1655ac35b7
SalosFB <samuel.cfb102009gmail.com>
2025-12-05 14:49:34 -0300

    Initial Commit

---
+++ Do-Re-Miau/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Do-Re-Miau
+//
+//  Created by User on 05/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Do-Re-Miau/Do_Re_MiauApp.swift
@@ -0,0 +1,17 @@
+//
+//  Do_Re_MiauApp.swift
+//  Do-Re-Miau
+//
+//  Created by User on 05/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Do_Re_MiauApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
