82a84ff15b2ae47e7bf032a61cd6fb2df12467bd
SalosFB <samuel.cfb102009gmail.com>
2025-12-09 16:24:38 -0300

    Tela de jogo refeita.

--- Do-Re-Miau/TelaDeJogo1.swift
+++ Do-Re-Miau/TelaDeJogo1.swift
@@ -9,9 +9,91 @@ import SwiftUI
 
 struct TelaDeJogo1: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        
+        ZStack {
+            Color(.corDeFundo)
+                .ignoresSafeArea()
+            
+            VStack {
+                HStack {
+                    //Botão
+                    Circle()
+                        .frame(height: 80)
+                    
+                    Spacer()
+                    
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 20)
+                            .fill(.amareloBarraDeTarefas)
+                            .frame(maxHeight: 80)
+                            .padding(.horizontal)
+                        
+                        Text("SOL LA SI DO DO RE MI")
+                            .font(.largeTitle)
+                            .padding()
+//                            .background()
+                            
+                    }
+                    
+                    Spacer()
+                        
+                    Circle()
+                        .frame(height: 80)
+                    
+                    
+                    Circle()
+                        .frame(height: 80)
+                }
+                .padding()
+
+                
+                RoundedRectangle(cornerRadius: 40)
+                    .fill(.white)
+                    .frame(maxHeight: 620)
+                
+                RoundedRectangle(cornerRadius: 100)
+                    .fill(.white)
+                    .frame(maxHeight: 201)
+                    .padding(.top)
+                    .overlay (
+                        HStack {
+                            //Botão
+                            
+                            //Titulo
+                            
+                            //Botão
+                            
+                            //Botão
+                        }
+                    )
+            }
+            .padding(.horizontal, 60)
+        }
+        
+        
+//        VStack(alignment: .center) {
+//            RoundedRectangle(cornerRadius: 20)
+//                .fill(Color.amareloBarraDeTarefas)
+//                .frame(height: 80)
+//                .padding(.horizontal, 250)
+//                .padding(.vertical, 10)
+//            RoundedRectangle(cornerRadius: 40)
+//                .fill(Color.brancoConfortavel)
+//                .padding(.horizontal, 145)
+//                .frame(height: 500)
+//            RoundedRectangle(cornerRadius: 200)
+//                .fill(Color.brancoConfortavel)
+//                .frame(height: 300)
+//                .padding(.horizontal, 80)
+//                .padding(.vertical, 10)
+//                .frame(height: 200, alignment: .top)
+//        }
+//        .frame(height: 900)
+//        .background(Color.corDeFundo)
     }
 }
+
 
 #Preview {
     TelaDeJogo1()
