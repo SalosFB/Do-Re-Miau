f7427ce1a16c082df9888ba19b42dec651f969ca
SalosFB <samuel.cfb102009gmail.com>
2025-12-09 15:20:02 -0300

    Fiz as formas da tela de jogo.

---
+++ Do-Re-Miau/TelaDeJogo1.swift
@@ -0,0 +1,18 @@
+//
+//  TelaDeJogo1.swift
+//  Do-Re-Miau
+//
+//  Created by User on 05/12/25.
+//
+
+import SwiftUI
+
+struct TelaDeJogo1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TelaDeJogo1()
+}
