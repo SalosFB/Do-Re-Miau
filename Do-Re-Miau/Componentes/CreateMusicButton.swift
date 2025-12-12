dd79ab11a18f25f3e463def3726c5efd9a2e0593
Nicolly <nicollysousace@gmail.com>
2025-12-12 16:32:26 -0300

    Feat: adicionar botão de controle de música e adicionar esse botão no componente de scroll view music note.

---
+++ Do-Re-Miau/Componentes/CreateMusicButton.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  Do-Re-Miau
+//
+//  Created by User on 12/12/25.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
