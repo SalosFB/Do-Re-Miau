11543cdd6c1cdeb7b3aad88449c909082451b391
Nicolly <nicollysousace@gmail.com>
2025-12-16 15:11:48 -0300

    Partitura: Adiciona layout da partitura

---
+++ Do-Re-Miau/Partitura.swift
@@ -0,0 +1,18 @@
+//
+//  Partitura.swift
+//  Do-Re-Miau
+//
+//  Created by User on 16/12/25.
+//
+
+import SwiftUI
+
+struct Partitura: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Partitura()
+}
