70c7c198872368c9dc73c90c12f405ed37569294
Nicolly <nicollysousace@gmail.com>
2025-12-19 15:35:06 -0300

    NoteMusicalModel:  Adiciona sons das notas musicais e corrige nome do enum para NoteMusicalModel. Pasta SoundEffects: Adiciona arquivos de aúdio.

--- Do-Re-Miau/ContentView.swift
+++ Do-Re-Miau/ContentView.swift
@@ -1,24 +1,13 @@
-//
-//  ContentView.swift
-//  Do-Re-Miau
-//
-//  Created by User on 05/12/25.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        ZStack {
+            Color.corDeFundo
+            ScoreMusicView()
+        }.ignoresSafeArea(.all)
     }
 }
-
 #Preview {
     ContentView()
 }
