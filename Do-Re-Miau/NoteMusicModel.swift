c7c7d34cebccd7fd1c3374f1d84bf60f6b13a7bf
Nicolly <nicollysousace@gmail.com>
2025-12-19 09:41:41 -0300

    NoteMusicalModel: Adiciona modelo da nota musical e reorganiza arquivo para pasta Features/

--- Do-Re-Miau/NoteMusicModel.swift
+++ Do-Re-Miau/NoteMusicModel.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+enum NotaMusicalModel: String, CaseIterable, Identifiable {
+    case doo, re, mi, fa, sol, la, si
+
+    var id: String { rawValue }
+
+    var nome: String {
+        rawValue.capitalized
+    }
+
+    var imagem: String {
+        "nota_\(rawValue)"
+    }
+
+    var som: String {
+        "som_\(rawValue)"
+    }
+
+}
