8af37fa20b9372eba720b080567d5e2140063397
Nicolly <nicollysousace@gmail.com>
2026-01-12 15:03:07 -0300

    PrologueView: Adicionar images e falas.

--- Do-Re-Miau/PrologueView.swift
+++ Do-Re-Miau/PrologueView.swift
@@ -24,9 +24,10 @@ struct Cena {
 struct PrologueView: View {
 
     let listaCenas = [
-        Cena(label: "Olá! blablbadlbald", imagePersonagem: "personagem_feliz", imageQuadro: "explicacao 1"),
-        Cena(label: "hahahahhaha", imagePersonagem: "personagem_pensativo", imageQuadro: "explicacao 2"),
-        Cena(label: "muahahhahah >:D", imagePersonagem: "personagem_alerta", imageQuadro: "explicacao 3")
+        Cena(label: "Ah, olá! Obrigado por vir. Precisava muito da sua ajuda! Vou te explicar o que está acontecendo.", imagePersonagem: "MirianImage", imageQuadro: "PartituraImage"),
+        Cena(label: "Eu preciso apresentar essa melodia para o recital de hoje, e essa apresentação vai ser muito importante para mim, pois é a minha primeira apresentação! Então para facilitar vou te explicar o que acontecendo na minha partitura.", imagePersonagem: "MirianImage", imageQuadro: "PartituraImage"),
+        Cena(label: "... AI… MEU… DEUS! Meu gato subiu e manchou minha partitura. Você pode me ajudar? Ainda dá pra ler a partitura, mas temos que estruturar tudo novamente até ficar bem claro para eu ler.", imagePersonagem: "MirianImage", imageQuadro: "PartituraImage"),
+        Cena(label: "Sim? Ainda bem, você é um ótimo amigo. Temos algumas maneiras para resolver este problema, escolha um modo.", imagePersonagem: "MirianImage", imageQuadro: "PartituraImage")
     ]
 
     @State var indiceAtual = 0
@@ -50,59 +51,84 @@ struct PrologueView: View {
         }
     }
     var body: some View {
-        ZStack {
-            Color.corBackground.ignoresSafeArea()
-            VStack {
-                HStack {
-                    //imagens da explicacao no 'quadro'
+
+        NavigationStack {
+            
+            ZStack {
+                Color.corBackground.ignoresSafeArea()
+                VStack {
+                    HStack {
+                        //imagens da explicacao no 'quadro'
+                        ZStack {
+                            Color.white.ignoresSafeArea()
+                            Image(listaCenas[indiceAtual].imageQuadro)
+                                .resizable()
+                                .scaledToFit()
+                        }
+                        .frame(width: 700, height: 500)
+                        .clipShape(RoundedRectangle(cornerRadius: 40))
+                        .padding(.trailing, 20)
+                        RoundedRectangle(cornerRadius: 10)
+                            .opacity(0)
+                            .frame(height: 1000)
+                            .overlay(Image(listaCenas[indiceAtual].imagePersonagem)
+                                .resizable()
+                                .scaledToFill()
+                            .scaleEffect(escalaPersonagem)
+                            .animation(.spring(response: 0.3, dampingFraction: 0.5), value: escalaPersonagem)
+                            ).padding(.trailing, 100)
+                        VStack {
+                            CreateNavigationButton(
+                                destination: MainView(),
+                                label: "",
+                                icon: "checkmark",
+                                widht: 80,
+                                height: 80,
+                                cornerRadius: 50,
+                                color: .corBotao,
+                                foregroundColor: .white,
+                                textSize: 20,
+                                imageSize: 1.0,
+                                action_status: {print("Indo para o menu..")}).padding(.top, 250)
+                            Spacer()
+                        }
+                    }
+                    Spacer()
+                }.padding(.bottom, 250).padding(.horizontal, 20)
+
+                VStack {
+                    Spacer()
+                    //Botao de pular/reiniciar
                     ZStack {
                         Color.white.ignoresSafeArea()
-                        Image(listaCenas[indiceAtual].imageQuadro)
+                        VStack{
+                            HStack {
+                                ZStack {
+                                    Color.corBotao.ignoresSafeArea()
+                                    Text("Miriam")
+                                        .font(.system(size: 20))
+                                        .foregroundStyle(Color.white)
+                                }.clipShape(RoundedRectangle(cornerRadius: 40))
+                                    .frame(width: 100, height: 35) //width está fixo
+                                Spacer()
+                            } .padding(20)
+                            Spacer()
+                            Text(listaCenas[indiceAtual].label)
+                                .lineLimit(nil)
+                                .fixedSize(horizontal: false, vertical: true)
+                                .padding(.horizontal, 10)
+                            Spacer()
+                            HStack {
+                                Spacer()
+                                CreateButton(label: (self.indiceAtual < self.listaCenas.count - 1) ? "Pular" : "Reiniciar", icon: "", widht: 100, height: 35, cornerRadius: 40, color: Color.corBotao, foregroundColor: Color.white, textSize: 20, imageSize: 0, action_status: proximaCena)
+                                
+                            } .padding(20)
+                        }
                     }
-                    .frame(width: 700, height: 500)
+                    .frame(height: 200)
                     .clipShape(RoundedRectangle(cornerRadius: 40))
-                    .padding(.trailing, 20)
-                    RoundedRectangle(cornerRadius: 10).frame(height: 1000).overlay(Image(listaCenas[indiceAtual].imagePersonagem)
-                        .scaleEffect(escalaPersonagem)
-                        .animation(.spring(response: 0.3, dampingFraction: 0.5), value: escalaPersonagem)
-                    ) //personagem provisorio
-                    VStack {
-                        CreateNavigationButton(destination: MainView(), label: "", icon: "checkmark", widht: 80, height: 80, cornerRadius: 50, color: Color.corBotao, foregroundColor: Color.white, textSize: 20, imageSize: 1.0, action_status: {print("Indo para o menu..")}).padding(.top, 250)
-                        Spacer()
-                    }
-                }
-                Spacer()
-            }.padding(.bottom, 250).padding(.horizontal, 20)
-
-            VStack {
-                Spacer()
-                //Botao de pular/reiniciar
-                ZStack {
-                    Color.white.ignoresSafeArea()
-                    VStack{
-                        HStack {
-                            ZStack {
-                                Color.corBotao.ignoresSafeArea()
-                                Text("Miriam")
-                                    .font(.system(size: 20))
-                                    .foregroundStyle(Color.white)
-                            }.clipShape(RoundedRectangle(cornerRadius: 40))
-                                .frame(width: 100, height: 35) //width está fixo
-                            Spacer()
-                        } .padding(20)
-                        Spacer()
-                        Text(listaCenas[indiceAtual].label)
-                        Spacer()
-                        HStack {
-                            Spacer()
-                            CreateButton(label: (self.indiceAtual < self.listaCenas.count - 1) ? "Pular" : "Reiniciar", icon: "", widht: 100, height: 35, cornerRadius: 40, color: Color.corBotao, foregroundColor: Color.white, textSize: 20, imageSize: 0, action_status: proximaCena)
-
-                        } .padding(20)
-                    }
-                }
-                .frame(height: 200)
-                .clipShape(RoundedRectangle(cornerRadius: 40))
-            }.padding(.horizontal, 20).padding(.bottom, 250)
+                }.padding(.horizontal, 20).padding(.bottom, 250)
+            }
         }
     }
 }
